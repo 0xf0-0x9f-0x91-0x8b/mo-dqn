cd6dcaa66df8fa43bef4de7356c39f792cf330d9
bhavini-jeloka <bhavini.jeloka@gmail.com>
2025-10-16 16:10:50 -0400

    all working

--- wrappers/linear_scalarization.py
+++ wrappers/linear_scalarization.py
@@ -1,5 +1,5 @@
 import numpy as np
-import gym
+import gymnasium as gym
 
 
 class LinearScalarization(gym.Wrapper):
@@ -9,13 +9,13 @@ class LinearScalarization(gym.Wrapper):
         self.weights = weights
         self.mo_return = None
 
-    def reset(self):
+    def reset(self, seed, options):
         o = super(LinearScalarization, self).reset()
         self.mo_return = 0.
         return o
 
     def step(self, a):
-        o, r, d, i = super(LinearScalarization, self).step(a)
+        o, r, d, t, i = super(LinearScalarization, self).step(a)
         self.mo_return += r
         scalarized = np.dot(r, self.weights)
-        return o, scalarized, d, i
+        return o, scalarized, d, t, i
